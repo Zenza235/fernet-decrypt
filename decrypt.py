ac3bf933a619b39709e202c0ef0b170ba1a4f621
Zenza235 <paoloatienza235@gmail.com>
2022-11-15 06:54:11 -0500

    init commit

---
+++ decrypt.py
@@ -0,0 +1,23 @@
+import sys
+
+from cryptography.fernet import Fernet
+
+if __name__ == '__main__':
+    print('Enter path of file to decrypt: ')
+    filepath = input()
+
+    print('Enter fernet key: ') # base 64 format
+    key = input()
+    fernet = Fernet(key)
+
+    with open(filepath, 'rb') as enc_file:
+        encrypted = enc_file.read()
+    
+    decrypted = fernet.decrypt(encrypted)
+    with open(filepath, 'wb') as dec_file:
+        dec_file.write(decrypted)
+    
+    print('File decrypted.')
+
+
+    
